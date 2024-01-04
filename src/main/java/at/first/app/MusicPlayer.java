c493624b0e771e138b731abda3eb856095210c06
Marat <davletshinm@list.ru>
2024-01-04 14:11:14 +0100

    Dependency Injection (DI) 3-nd lesson

--- src/main/java/at/first/app/MusicPlayer.java
+++ src/main/java/at/first/app/MusicPlayer.java
@@ -1,14 +1,38 @@
 package at.first.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
+
+    public String getName() {return name;}
+
+    public void setName(String name) { this.name = name;}
+
+    public int getVolume() { return volume; }
+
+    public void setVolume(int volume) { this.volume = volume; }
 
     // IoC = Inversion of control
-    public MusicPlayer(Music music){
-        this.music = music;
+    public MusicPlayer(List<Music> musicList){
+        this.musicList = musicList;
+    }
+
+    public MusicPlayer() {}
+
+    // <property name="music" ref="musicBean"/>
+    // Spring make setMusic to "music"
+    public void setMusic(List<Music> musicList){
+        //this.music = music;
+        this.musicList = musicList;
     }
 
     public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
--- src/main/java/at/first/app/TestSpring.java
+++ src/main/java/at/first/app/TestSpring.java
@@ -16,18 +16,34 @@ public class TestSpring {
         // applicationContext should be closed
         context.close();
         // todo 1 lesson done
-         */
+
+        // todo 2nd lesson start
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
         // ("Bean ID" in the applicationContext.xml, class in project)
         Music music= context.getBean("musicBean", Music.class);
-
         MusicPlayer musicPlayer = new MusicPlayer(music);
         musicPlayer.playMusic();
-
         // applicationContext should be closed
         context.close();
+        //todo 2nd lesson is done
+         */
+
+        // todo 3rd lesson start
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+        // applicationContext should be closed
+        context.close();
+        //todo 3rd lesson is done
+
+
+
 
 
 
